c2add8c8c32eea55ac232b79d7cb975875510f97
João Paulo <157292969+jotape-c@users.noreply.github.com>
2024-04-07 15:07:52 +0000

    composer.yaml utilizando variaveis de ambiente

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
   const result = await client.query(queryObject);
