75429394ba09c7df26ce8d1675427cc70ceb8460
João Paulo <157292969+jotape-c@users.noreply.github.com>
2024-04-08 02:25:12 +0000

    testing date

--- pages/api/v1/status.js
+++
@@ -1,9 +0,0 @@
-import database from "infra/database.js";
-
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).send("Filipe, fora de serie!");
-}
-
-export default status;
