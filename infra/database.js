af592a86ad56135a58daa5e4c2b9cb636dc5fd7c
João Paulo <157292969+jotape-c@users.noreply.github.com>
2024-04-13 14:50:57 +0000

    investigando a conexao

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
   });
 
+  console.log("Credenciais do Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -16,6 +24,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
