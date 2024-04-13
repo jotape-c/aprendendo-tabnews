f6d6af414b1e488df4e582b3f3acf77662e27f55
João Paulo <157292969+jotape-c@users.noreply.github.com>
2024-04-13 18:26:20 +0000

    more robust dabtabase connection

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
-  console.log("Credenciais do Postgres: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    ssl: getSSLValue(),
   });
 
   try {
@@ -34,3 +26,10 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValue() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+  process.env.NODE_ENV === "development" ? false : true;
+}
